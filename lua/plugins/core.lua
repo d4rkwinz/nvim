895053559885713d55457fab1b9c2668f4a0ea88
d4rkwinz <d4rkwinz@gmail.com>
2022-11-25 20:22:11 +0700

    feat: add lsp, coding

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -118,19 +118,6 @@ return {
   -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-      },
-    },
-  },
-
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
