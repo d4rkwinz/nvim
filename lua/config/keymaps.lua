fed8a61046e865b2eca8f17232fff28ad06a08a3
d4rkwinz <d4rkwinz@gmail.com>
2022-12-03 20:22:11 +0700

    feat: add keymappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,88 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+---
+local Util = require("lazyvim.util")
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+-- Insert --
+-- Press jj,jk fast to enter
+keymap("i", "jj", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", opts)
+
+-- Close buffers
+if Util.has("mini.bufremove") then
+  keymap("n", "<S-q>", function()
+    require("mini.bufremove").delete(0, false)
+    local bufs = vim.fn.getbufinfo({ buflisted = true })
+    -- open alpha if no buffers are left
+    if not bufs[2] and Util.has("alpha-nvim") then
+      require("alpha").start(true)
+    end
+  end, opts)
+else
+  keymap("n", "<S-q>", "<cmd>bd<CR>", opts)
+end
+
+-- Copy whole file content to clipboard with C-c
+keymap("n", "<C-c>", ":%y+<CR>", opts)
+
+-- Move live up or down
+-- moving
+keymap("n", "<A-Down>", ":m .+1<CR>", opts)
+keymap("n", "<A-Up>", ":m .-2<CR>", opts)
+keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
+
+-- NullLs Info keymap
+if Util.has("null-ls.nvim") then
+  keymap("n", "<leader>cn", "<cmd>NullLsInfo<CR>", opts)
+end
+
+-- LspSaga
+if Util.has("lspsaga.nvim") then
+  -- when you use action in finder like open vsplit then you can
+  keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+  -- Code action
+  keymap({ "n", "v" }, "gla", "<cmd>Lspsaga code_action<CR>")
+
+  -- Rename all occurrences of the hovered word for the entire file
+  keymap("n", "glr", "<cmd>Lspsaga rename<CR>")
+
+  -- Rename all occurrences of the hovered word for the selected files
+  keymap("n", "glR", "<cmd>Lspsaga rename ++project<CR>")
+
+  -- Peek definition
+  keymap("n", "gld", "<cmd>Lspsaga peek_definition<CR>")
+
+  -- Diagnostic jump can use `<c-o>` to jump back
+  keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+  keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+  -- Diagnostic jump with filters such as only jumping to an error
+  keymap("n", "[E", function()
+    require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+  end)
+  keymap("n", "]E", function()
+    require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+  end)
+
+  -- Toggle Outline
+  keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+
+  -- Callhierarchy
+  keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+  keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+end
+
+-- Trouble
+-- Add keymap only show FIXME
+if Util.has("todo-comments.nvim") then
+  -- show fixme on telescope
+  keymap("n", "<leader>xf", "<cmd>TodoTelescope keywords=FIX,FIXME<CR>")
+end
