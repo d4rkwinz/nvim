9f496807476864faa44ffdc6e48e45fc7a4ba55f
d4rkwinz <d4rkwinz@gmail.com>
2023-10-06 22:22:22 +0700

    feat: lazyvim starter

---
+++ lua/config/neovide.lua
@@ -0,0 +1,11 @@
+-- Set GUI font
+vim.o.guifont = 'FiraCode Nerd Font:h12' -- Adjust to your desired font and size
+
+-- Neovide-specific settings
+vim.g.neovide_cursor_vfx_mode = 'railgun' -- Cool cursor effects
+vim.g.neovide_transparency = 0.9 -- Transparency (optional)
+vim.g.neovide_fullscreen = true -- Start in fullscreen mode
+
+-- Optional performance tweaks
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate_idle = 5
